1b93d8781f525c91d4f42d3a130b399b5585780a
Lin Yi Jun <z66500646@gmail.com>
2023-09-05 21:52:35 +0800

    練習-代辦事項Toggle功能

--- next-base/components/note/index.js
+++ next-base/components/note/index.js
@@ -1,6 +1,12 @@
 import { useState } from 'react';
+//引入style檔案
+import styles from './note.module.css';
 
 export default function NoteIndex() {
+  //全選使用狀態
+  //藉由e.target確認是true/false
+  // const [selected, setSelectAll] = useState('');
+
   //input輸入框使用狀態
   const [inputValue, setInputValue] = useState('');
 
@@ -31,7 +37,7 @@ export default function NoteIndex() {
     // 從ids中找出最大值
     const newId = todos.length > 0 ? Math.max(...ids) + 1 : 1;
     // text?
-    const newTodo = { id: newId, text: text };
+    const newTodo = { id: newId, text: text, completed: false };
     return [newTodo, ...todos];
   };
 
@@ -40,12 +46,33 @@ export default function NoteIndex() {
     //移除指定id
     return todos.filter((v) => v.id !== id);
   };
+  //切換toggle checkbox狀態
+  const toggleCompleted = (todos, id) => {
+    return todos.map((v) => {
+      //淺拷貝v屬性的值，並且將completed的值變成相反
+      if (v.id === id) return { ...v, completed: !v.completed };
+      else return { ...v };
+    });
+  };
+  //切換toggle 全選狀態
+  const toggleSelectedAll = (todos, isselectedAll) => {
+    return todos.map((v) => {
+      // 將completed改成isselectedAll狀態
+      return { ...v, completed: isselectedAll };
+    });
+  };
   //   ----------------------------------------
   const handleAdd = (text) => {
     setTodos(add(todos, text));
   };
   const handleRomove = (id) => {
     setTodos(remove(todos, id));
+  };
+  const handleToggleCompleted = (id) => {
+    setTodos(toggleCompleted(todos, id));
+  };
+  const handleToggleSelectedAll = (isselectedAll) => {
+    setTodos(toggleSelectedAll(todos, isselectedAll));
   };
   //   ----------------------------------------
   //顯示內容return
@@ -70,17 +97,47 @@ export default function NoteIndex() {
           }
         }}
       />
+      <div>
+        {/* 全選功能 */}
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          // 全選使用狀態
+          //用於檢查checkbox在checked狀態時的屬性
+          // 如果已勾選，則回傳值true
+          // 如果未勾選，則回傳值false
+          // setSelectAll(e.target.checked);
+
+          handleToggleSelectedAll(e.target.checked);
+        }}
+      />
+      全選
+      </div>
       {/* list */}
       <ul>
+      
         {todos.map((v, i) => {
           return (
             <li key={v.id}>
-              <input type="checkbox" />
-              {v.text}
+              <input
+                type="checkbox"
+                checked={v.completed}
+                onChange={() => {
+                  //切換checkbox狀態
+                  handleToggleCompleted(v.id);
+                }}
+              />
+              {/* 判斷屬性completed來執行css的className */}
+              <span
+                className={v.completed ? styles['completed'] : styles['active']}
+              >
+                {v.text}
+              </span>
+
               {/* Remove功能 */}
               <button
                 onClick={() => {
-                  handleRomove(v.id)
+                  handleRomove(v.id);
                 }}
               >
                 X
